ff38b5133ae105f25cfbb36c070c65919282e460
jhonuru <47787645+jhonuru@users.noreply.github.com>
2024-08-21 12:39:38 -0500

    Avance 3

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -2,10 +2,184 @@
 .curso-main-container.pb-3
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
-    .titulo-principal.color-acento-contenido
+    .titulo-principal.color-acento-contenido(data-aos="flip-up")
       .titulo-principal__numero
         span 1
-      h1 Titulo de primer nivel
+      h1 Generalidades
+    .row.align-items-center.mb-5
+      .col-lg-7.mb-4.mb-lg-0(data-aos="fade-right")
+        p La agricultura es el conjunto de actividades que se llevan a cabo para producir especies vegetales para el beneficio del hombre. Hay multitud de opciones para practicar la agricultura; estas opciones se llaman #[b "Modelos de producción"].
+        p.mb-0 Uno de los modelos de producción agrícola es el llamado #[b "Revolución Verde"], implementado desde la década de los 70s. Este modelo, en términos generales, involucra:
+      .col-lg-5(data-aos="fade-left")
+        figure
+          img(src='@/assets/curso/tema1/img1.png', style="width: 500px", alt='').m-auto
+    .row.mb-0.mb-lg-5(data-aos="fade-down")
+      .col-6.col-md-4.col-lg-2.mb-4.mb-lg-0
+        .p-4.bg-c4
+          figure.mb-3
+            img(src='@/assets/curso/tema1/img1.svg', style="width: 80px", alt='').m-auto
+          p.text-center.mb-0 Alto uso de agroquímicos de síntesis.
+      .col-6.col-md-4.col-lg-2.mb-4.mb-lg-0
+        .p-4.bg-c4
+          figure.mb-3
+            img(src='@/assets/curso/tema1/img2.svg', style="width: 60px", alt='').m-auto
+          p.text-center.mb-0 Modificaciones genéticas.
+      .col-6.col-md-4.col-lg-2.mb-4.mb-lg-0
+        .p-4.bg-c4
+          figure.mb-3
+            img(src='@/assets/curso/tema1/img3.svg', style="width: 80px", alt='').m-auto
+          p.text-center.mb-0 Utilización intensiva del recurso del suelo.
+      .col-6.col-md-4.col-lg-2.mb-4.mb-lg-0
+        .p-4.bg-c4
+          figure.mb-3
+            img(src='@/assets/curso/tema1/img4.svg', style="width: 80px", alt='').m-auto
+          p.text-center.mb-0 Especialización de la producción.
+      .col-6.col-md-4.col-lg-2.mb-4.mb-lg-0
+        .p-4.bg-c4
+          figure.my-3
+            img(src='@/assets/curso/tema1/img5.svg', style="width: 80px", alt='').m-auto
+          p.text-center.mb-0 Monocultivo.
+      .col-6.col-md-4.col-lg-2.mb-4.mb-lg-0
+        .p-4.bg-c4
+          figure.mb-3
+            img(src='@/assets/curso/tema1/img6.svg', style="width: 80px", alt='').m-auto
+          p.text-center.mb-0 Mecanización.
+    p.mb-5(data-aos="fade-right") La acción de este modelo en sistemas agrícolas ha causado graves efectos negativos medioambientales, sociales y económicos.
+    .row.bg-c5.mb-5(data-aos="fade-left")
+      .col-12
+        .px-5
+          .row.align-items-center
+            .col-lg-5.mb-4.mb-lg-0
+              figure.mt-4.mt-lg-0
+                img(src='@/assets/curso/tema1/img3.png', style="width: 500px", alt='').m-auto
+            .col-lg-7
+              p.mb-0 Ante esta situación, se vienen proponiendo modelos alternativos más amigables con el medio ambiente en el marco de una producción sostenible. Uno de ellos es el de la agricultura ecológica, basado en los principios de la agroecología como ciencia.
+    .row.mb-5(data-aos="fade-right")
+      .col-auto.bg-c6
+        .row.align-items-end
+          .col-auto.pe-0
+            figure
+              img(src='@/assets/curso/tema1/img8.svg', style="width: 60px", alt='').m-auto
+          .col
+            h2.mb-1 Conceptos básicos
+    .row.mb-5
+      .col-lg-6.mb-4.mb-lg-0(data-aos="fade-right")
+        .bloque-texto-c.color-secundario.p-4
+          i.fas.fa-quote-right.mb-4
+          h3.mb-2 "La agroecología como disciplina científica estudia los componentes de un sistema agrícola, sus interrelaciones y la manera de intervenirlo con prácticas que promuevan su sostenibilidad".
+          span (FAO, s.f.).
+      .col-md-6.col-lg-3.mb-4.mb-lg-0(data-aos="fade-down")
+        figure
+          img(src='@/assets/curso/tema1/img4.png', style="width: 300px", alt='').m-auto
+      .col-md-6.col-lg-3(data-aos="fade-left")
+        figure
+          img(src='@/assets/curso/tema1/img9.svg', style="width: 300px", alt='').m-auto
+    p.mb-5(data-aos="fade-right") Alrededor del tema de la agroecología se manejan diversos conceptos, sobre los cuales es necesario hacer claridad a fin de utilizarlos adecuadamente y conocer su alcance.
+    .row.justify-content-center.mb-5
+      .col-lg-10(data-aos="fade-down")
+        .row.justify-content-center
+          .col-10.col-md-8.col-lg-2.bg-c7
+            .p-4
+              figure
+                img(src='@/assets/curso/tema1/img10.svg', style="width: 100px", alt='').m-auto
+          .col-10.col-md-8.col-lg-10.bg-c8
+            .p-4.h-100
+              .row.align-items-center.h-100
+                .col-12
+                  p.mb-0 #[b La agricultura ecológica] es un modelo de producción basado en la agroecología. Se fundamenta en el agroecosistema y su manejo para producción ecológica.
+    p.mb-5(data-aos="fade-right") Las prácticas de la agricultura ecológica son:
+    .row.justify-content-center.mb-5
+      .col-10.col-md-5.col-lg.m-2(data-aos="fade-right")
+        .row
+          .col-12.px-0
+            figure
+              img(src='@/assets/curso/tema1/img5.png', style="width: 330px", alt='').m-auto
+          .col-12.bg-c9
+            .p-4
+              .row.mb-4
+                .col-12
+                  .p-3.bg-white
+                    p.text-center.mb-0 #[b Uso de coberturas muertas]
+              p.text-center.mb-0 Utilización de residuos vegetales como protección del suelo.
+      .col-10.col-md-5.col-lg.m-2(data-aos="fade-down")
+        .row
+          .col-12.px-0
+            figure
+              img(src='@/assets/curso/tema1/img6.png', style="width: 330px", alt='').m-auto
+          .col-12.bg-c9
+            .p-4
+              .row.mb-4
+                .col-12
+                  .p-3.bg-white
+                    p.text-center.mb-0 #[b Uso de coberturas vivas en el suelo]
+              p.text-center.mb-0 Cultivos rastreros o de porte bajo para proteger el suelo.
+      .col-10.col-md-5.col-lg.m-2(data-aos="fade-up")
+        .row
+          .col-12.px-0
+            figure
+              img(src='@/assets/curso/tema1/img7.png', style="width: 330px", alt='').m-auto
+          .col-12.bg-c9
+            .p-4
+              .row.mb-4
+                .col-12
+                  .p-3.bg-white
+                    p.text-center.mb-0 #[b Control biológico]
+              p.text-center.mb-0 Empleo de organismos reguladores de plagas y enfermedades.
+      .col-10.col-md-5.col-lg.m-2(data-aos="fade-left")
+        .row
+          .col-12.px-0
+            figure
+              img(src='@/assets/curso/tema1/img8.png', style="width: 330px", alt='').m-auto
+          .col-12.bg-c9
+            .p-4
+              .row.mb-4
+                .col-12
+                  .p-3.bg-white
+                    p.text-center.mb-0 #[b Uso de biopreparados]
+              p.text-center.mb-0 Preparaciones biológicas para mejorar la salud del suelo y las plantas.
+    p.mb-5(data-aos="fade-right") Algunos de los modelos de agricultura ecológica son:
+    .row.justify-content-center.mb-5
+      .col-lg-10(data-aos="fade-down")
+        .row
+          .col-lg-4.bg-c7
+            .p-3
+              .row.justify-content-center
+                .col-10.mb-3
+                  .p-3.bg-c8
+                    p.text-center.mb-0 #[b Agricultura orgánica]
+                .col-10.mb-3
+                  .p-3.bg-c8
+                    p.text-center.mb-0 #[b Agricultura biológica]
+                .col-10
+                  .p-3.bg-c8
+                    p.text-center.mb-0 #[b Agricultura natural]
+          .col-lg-8.bg-c3
+            .p-3.h-100
+              .row.align-items-center.h-100
+                .col
+                  p.mb-0 Todos ellos trabajan con el concepto de agroecosistema, que se refiere al conjunto de seres vivos que interactúan en un medio de producción para beneficio del hombre. De igual forma, estos enfoques se enmarcan en el concepto de sostenibilidad, lo cual significa que la producción, en este caso la agrícola, se sostenga en el tiempo en adecuadas condiciones ambientales, sociales y económicas.
+                .col-md-auto
+                  figure
+                    img(src='@/assets/curso/tema1/img11.svg', style="width: 140px", alt='').m-auto
+    .row
+      .col-lg-8.order-2.order-lg-1(data-aos="fade-right")
+        .p-4.bg-c10.h-100
+          .row.align-items-center
+            .col-lg-4
+              figure
+                img(src='@/assets/curso/tema1/img12.svg', style="width: 300px", alt='').m-auto
+            .col-lg-8
+              p #[b Pódcast]
+              p Es momento de escuchar el podcast de Don Campos y Azusena. En él, explican cómo los modelos alternativos de agroecología están transformando la agricultura hacia una mayor sostenibilidad, respetando y colaborando con los ecosistemas naturales.
+              TarjetaAudio.color-primario.mb-3(
+                texto="Modelos alternativos de agroecología"
+                :audio="require('@/assets/componentes/audios/audio-ej.mp3')"
+                @audio-hover="mostrarIndicadorTarjetaAudio = false"
+              )
+                .indicador--click(v-if="mostrarIndicadorTarjetaAudio")
+      .col-lg-4.mb-4.mb-lg-0.order-1.order-lg-2(data-aos="fade-left")
+        figure
+          img(src='@/assets/curso/tema1/img9.png', style="width: 360px", alt='').m-auto
       
 </template>
 
@@ -14,7 +188,7 @@ export default {
   name: 'Tema1',
   components: {},
   data: () => ({
-    // variables de vue
+    mostrarIndicadorTarjetaAudio: true,
   }),
   mounted() {
     this.$nextTick(() => {
